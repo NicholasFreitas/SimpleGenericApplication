887ed0a77c2a5387a555ff4941545e912301ef48
Nicholas Freitas <nicholasfreitas@sympatico.ca>
2019-04-17 20:14:07 -0400

    Simple Generic App initial commit
    The basic app. As I continue the pluralsight course on this topic, this application will change.

---
+++ BufferExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BufferExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = new CircularBuffer<double>(capacity:5);
+
+            while (true)
+            {
+                
+                var value = 0.0;
+                var input = Console.ReadLine();
+                
+                if (double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+
+                break;
+
+            }
+
+
+            var sum = 0.0;
+            Console.WriteLine("Buffer: ");
+            while (!buffer.IsEmpty)
+            {
+                sum += buffer.Read();
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
